7f69ae67b8a03b0dc71ade517ced8718cad3c5de
channasorn chungmarisakul <63010185@KMITL.AC.TH>
2020-09-13 18:48:06 +0700

    ADD USELESS FUNCTION

--- WORK5.1/Source.cpp
+++ WORK5.1/Source.cpp
@@ -1,9 +1,13 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+void Aa();
 char* p, str[100];
 int main(){
 	p = str;
-	scanf("%s", str, 99);//ป้อนได้ไม่เกิน9ตัว
+	scanf("%s", str, 99);//ป้อนได้ไม่เกิน99ตัว
+	Aa();
+}
+void Aa() {
 	while (*p != '\0') {
 		if (*p >= 65 && *p <= 90) {
 			printf("%c", *p);
@@ -11,7 +15,6 @@ int main(){
 		if (*p >= 97 && *p <= 122) {
 			printf("%c", *p);
 		}
-			p++;
+		p++;
 	}
-
 }
